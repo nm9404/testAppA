ed085475e6926e4153473eefed89987ad7a3e42f
nm9404 <nmunerag@unal.edu.co>
2019-03-04 11:15:34 -0500

    Agregadas index y edit y links de navegacion

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,20 +3,40 @@ class PostsController < ApplicationController
     @post = Post.new
   end
   
+  def index
+    @posts = Post.all #obtener todos los articulos
+    
+  end
+  
+  def edit
+    @post = Post.find(params[:id])
+  end
+  
+  def update
+    @post = Post.find(params[:id]) #primero localizar el articulo
+    if @post.update(post_params)
+      flash[:notice] = "El post se actualiza satisfactoriamente"
+      redirect_to post_path(@post) 
+      #render 'new'
+    else
+      render 'edit'
+    end
+  end
+  
   def create
     #render plain: params[:post].inspect
     @post = Post.new(post_params)
     if @post.save
       flash[:notice] = "El post se creo satisfactoriamente"
-      redirect_to posts_path(@post)
+      redirect_to post_path(@post) 
+      #render 'new'
     else
       render 'new'
-    end
-    
+    end 
   end
   
   def show
-    @post = Post.find(param[:id]) #aqui mostramos el id    
+    @post = Post.find(params[:id]) #aqui mostramos el id    
   end
   
   private
